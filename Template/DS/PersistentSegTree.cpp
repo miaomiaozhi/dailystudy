1549ff7fdf8a7c492fe6ce26477277cf86f09760
miaomiaozhi <76694503+miaomiaozhi@users.noreply.github.com>
2022-10-17 16:53:24 +0800

    Update PersistentSegTree.cpp

--- Template/DS/PersistentSegTree.cpp
+++ Template/DS/PersistentSegTree.cpp
@@ -1,56 +1,59 @@
-struct ChairManTree {
-    const int inf = 0x3f3f3f3f;
-
-    struct node {
-        int l = 0, r = 0;
-        LL val = 0;
-    };
-    
-    int n, idx;
+template <class Info>
+struct PersistentSegTree {
+    int n, N;
     vector <int> root;
-    vector <node> tr;
-    ChairManTree() {}
-    ChairManTree(int _n) : n(_n), idx(0), root(n + 5), tr((n + 5) << 5) {}
-    ChairManTree(int _n, const vector <int> &a) : ChairManTree(_n) {
-        for (int i = 1; i <= n; i++) {
-            insert(root[i], root[i - 1], 0, inf, a[i]);
-        }
+    vector <Info> tr {{}, {}};
+
+    PersistentSegTree() = default;
+    PersistentSegTree(int _n, int _N) : n(_n), N(_N), root(n + 5) {
+        tr.reserve(n << 6);
     }
-    void newNode(int &u) {
-        u = ++idx;
+    int newNode() {
+        tr.push_back(tr[0]);
+        return (int) tr.size() - 1;
     }
     void pushup(int u) {
-        tr[u].val = tr[tr[u].l].val + tr[tr[u].r].val;
+        // @TODO: pushup
+        tr[u].cnt = tr[tr[u].lc].cnt + tr[tr[u].rc].cnt;
     }
-    void insert(int &u, int v, int l, int r, int val) {
-        newNode(u);
+    int insert(int v, int l, int r, int val) {
+        int u = newNode();
         tr[u] = tr[v];
         if (l == r) {
-            tr[u].val += val;
-            return;
-        }
-        int mid = l + r >> 1;
-        if (val <= mid) {
-            insert(tr[u].l, tr[v].l, l, mid, val);
+            // @TODO: Leaf
+            tr[u].cnt += 1;
         } else {
-            insert(tr[u].r, tr[v].r, mid + 1, r, val);
+            int mid = l + r >> 1;
+            if (val <= mid) {
+                tr[u].lc = insert(tr[v].lc, l, mid, val);
+            } else {
+                tr[u].rc = insert(tr[v].rc, mid + 1, r, val);
+            }
+            pushup(u);
         }
-        pushup(u);
+        return u;
     }
-    LL query(int u, int v, int l, int r, LL val) {
-        if (!u && !v) {
-            return 0;
+    void insert(int i, int val) {
+        root[i] = insert(root[i - 1], -N, N, val);
+    }
+    int query(int u, int v, int l, int r, int k) {
+        if (l == r) {
+            return r;
         }
-        if (r <= val) {
-            return tr[u].val - tr[v].val;
-        }
-        if (l > val) {
-            return 0;
-        }
+        int cnt = tr[tr[u].lc].cnt - tr[tr[v].lc].cnt;
         int mid = l + r >> 1;
-        return query(tr[u].l, tr[v].l, l, mid, val) + query(tr[u].r, tr[v].r, mid + 1, r, val);
+        if (k <= cnt) {
+            return query(tr[u].lc, tr[v].lc, l, mid, k);
+        } else {
+            return query(tr[u].rc, tr[v].rc, mid + 1, r, k - cnt);
+        }
     }
-    LL query(int l, int r, LL val) {
-        return query(root[r], root[l - 1], 0, inf, val);
+    int query(int l, int r, int k) {
+        return query(root[r], root[l - 1], -N, N, k);
     }
+};
+
+struct Info {
+    int lc = 0, rc = 0;
+    int cnt = 0;
 };
